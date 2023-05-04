c996060daba872b268c52710ccd17d2a66433d4d
caue-santos-axon <caue.santos@axontechnology.com.br>
2023-05-04 17:01:36 -0300

    feat: add label to server status

--- internal/gui/landing/landing.go
+++ internal/gui/landing/landing.go
@@ -19,7 +19,7 @@ type RenderLanding struct {
 var registerWindow fyne.Window
 var settingsWindow fyne.Window
 
-func (r *RenderLanding) RenderLandingWindow(a fyne.App, name string, status bool, config *settings.Configs, accounts []settings.Account) fyne.Window {
+func (r *RenderLanding) RenderLandingWindow(a fyne.App, name string, service_status bool, server_status bool, config *settings.Configs, accounts []settings.Account) fyne.Window {
 
 	w := a.NewWindow("Prox Client")
 	w.Resize(fyne.NewSize(500, 500))
@@ -38,28 +38,39 @@ func (r *RenderLanding) RenderLandingWindow(a fyne.App, name string, status bool
 		},
 	)
 
-	heightAux := float32(10)
+	yAxisAuxliar := float32(10)
 
 	label_name := canvas.NewText(name, color.Black)
 	label_name.Resize(fyne.NewSize(480, 30))
 	label_name.TextSize = 24
 	label_name.Alignment = fyne.TextAlignCenter
-	label_name.Move(fyne.NewPos(10, heightAux))
-	heightAux = heightAux + label_name.MinSize().Height + 10
+	label_name.Move(fyne.NewPos(10, yAxisAuxliar))
+	yAxisAuxliar = yAxisAuxliar + label_name.MinSize().Height + 10
 
-	icon_status := canvas.NewCircle(color.NRGBA{R: 0, G: 255, B: 0, A: 255})
-	icon_status.Resize(fyne.NewSize(12, 12))
-	icon_status.Move(fyne.NewPos(10, heightAux+10)) // "+10" to fit circle behavior
+	service_icon_status := canvas.NewCircle(color.NRGBA{R: 0, G: 255, B: 0, A: 255})
+	service_icon_status.Resize(fyne.NewSize(12, 12))
+	service_icon_status.Move(fyne.NewPos(10, yAxisAuxliar+10)) // "+10" to fit circle behavior
 
-	label_status := widget.NewLabel("Online")
-	label_status.Resize(fyne.NewSize(240, 30))
-	label_status.Move(fyne.NewPos(25, heightAux))
+	service_label_status := widget.NewLabel("Prox Service running")
+	service_label_status.Resize(fyne.NewSize(240, 30))
+	service_label_status.Move(fyne.NewPos(18, yAxisAuxliar))
 
-	if !status {
-		icon_status.FillColor = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
-		icon_status.Refresh()
-		label_status.SetText("Offline")
-		label_status.Refresh()
+	if !service_status {
+		service_icon_status.FillColor = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+		service_label_status.SetText("Prox Service not running")
+	}
+
+	server_icon_status := canvas.NewCircle(color.NRGBA{R: 0, G: 255, B: 0, A: 255})
+	server_icon_status.Resize(fyne.NewSize(12, 12))
+	server_icon_status.Move(fyne.NewPos(220, yAxisAuxliar+10)) // "+10" to fit circle behavior
+
+	server_label_status := widget.NewLabel("Prox server running")
+	server_label_status.Resize(fyne.NewSize(100, 30))
+	server_label_status.Move(fyne.NewPos(228, yAxisAuxliar))
+
+	if !server_status {
+		server_icon_status.FillColor = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+		server_label_status.SetText("Prox server not running")
 	}
 
 	btn_options := widget.NewButton("", func() {
@@ -71,15 +82,15 @@ func (r *RenderLanding) RenderLandingWindow(a fyne.App, name string, status bool
 		settingsWindow = s.RenderSettingsWindow(config, a)
 	})
 	btn_options.Icon = theme.SettingsIcon()
-	btn_options.Move(fyne.NewPos(424, heightAux))
+	btn_options.Move(fyne.NewPos(424, yAxisAuxliar))
 	btn_options.Resize(fyne.NewSize(50, 40))
 	btn_options.Importance = widget.LowImportance
 	btn_options.Refresh()
-	heightAux = heightAux + label_status.MinSize().Height + 10
+	yAxisAuxliar = yAxisAuxliar + service_label_status.MinSize().Height + 10
 
 	label_accounts := canvas.NewText("Clientes", color.Black)
 	label_accounts.TextSize = 18
-	label_accounts.Move(fyne.NewPos(17, heightAux))
+	label_accounts.Move(fyne.NewPos(17, yAxisAuxliar))
 
 	btn_new_account := widget.NewButton("Novo", func() {
 		r := guir.RenderRegister{}
@@ -89,22 +100,24 @@ func (r *RenderLanding) RenderLandingWindow(a fyne.App, name string, status bool
 		registerWindow = r.RenderNewAccountWindow(config, accounts, a, list)
 	})
 	btn_new_account.Resize(fyne.NewSize(50, 30))
-	btn_new_account.Move(fyne.NewPos(424, heightAux))
+	btn_new_account.Move(fyne.NewPos(424, yAxisAuxliar))
 	btn_new_account.Importance = widget.LowImportance
 	btn_new_account.Refresh()
-	heightAux = heightAux + btn_new_account.MinSize().Height + 10
+	yAxisAuxliar = yAxisAuxliar + btn_new_account.MinSize().Height + 10
 
 	container_accordion := container.NewVScroll(
 		list,
 	)
 	container_accordion.Resize(fyne.NewSize(464, 330))
-	container_accordion.Move(fyne.NewPos(10, heightAux))
+	container_accordion.Move(fyne.NewPos(10, yAxisAuxliar))
 	//heightAux = heightAux + accordion_accounts.MinSize().Height + 10
 
 	wrapperContainer := container.NewWithoutLayout(
 		label_name,
-		icon_status,
-		label_status,
+		service_icon_status,
+		service_label_status,
+		server_icon_status,
+		server_label_status,
 		btn_options,
 		label_accounts,
 		btn_new_account,
