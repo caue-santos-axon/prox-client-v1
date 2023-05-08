4576ef44dae24b542a7ff2421064322c7bd78323
caue-santos-axon <caue.santos@axontechnology.com.br>
2023-05-08 12:32:53 -0300

    feat: add mutext pacakge

---
+++ internal/mutex/mutex.go
@@ -0,0 +1,29 @@
+package mutex
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+var (
+	kernel32        = syscall.NewLazyDLL("kernel32.dll")
+	procCreateMutex = kernel32.NewProc("CreateMutexW")
+)
+
+func CreateMutex(name string) (uintptr, error) {
+	p, err := syscall.UTF16PtrFromString(name)
+	if err != nil {
+		return 0, err
+	}
+	ret, _, err := procCreateMutex.Call(
+		0,
+		0,
+		uintptr(unsafe.Pointer(p)),
+	)
+	switch int(err.(syscall.Errno)) {
+	case 0:
+		return ret, nil
+	default:
+		return ret, err
+	}
+}
